9516d6ddfd70d33997f4a06bb68b2883bdd571ea
Kaia Hellem <kaia.hellem@gmail.com>
2026-06-10 08:29:54 +0200

    Add navigation between pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main>
       <h1>Kaia Hellem</h1>
       <p>Velkommen til porteføljen min</p>
+      <Link href="/om">Om meg</Link>
     </main>
   )
 }
