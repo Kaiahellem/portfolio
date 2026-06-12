54d8c1ae248684f3893497ea9cb0421fba522f95
Kaia Hellem <kaia.hellem@gmail.com>
2026-06-12 05:14:37 +0200

    Move navigation to layout

--- app/om/page.tsx
+++ app/om/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
 
 export default function Om() {
 return (
     <main>
         <h1>Om meg</h1>
         <p>Her kommer det litt om hvem jeg er</p>
-        <Link href="/">Hjem</Link>
     </main>
 );
 }
