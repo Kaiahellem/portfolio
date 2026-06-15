d28f1d55cd6b4a10c036ca110330bc70b47abcf5
Kaia Hellem <kaia.hellem@gmail.com>
2026-06-15 20:07:33 +0200

    Add pageLayout to about and projects page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,14 @@
+import PageLayout from "../components/PageLayout";
 
 export default function About() {
 return (
-    <main>
-        <h1>Om meg</h1>
+    <PageLayout title={<h1>Om meg</h1>}>
         <p>Hei!</p>
         <p>Jeg er Kaia, en utvikler med sans for å både bygge ting som funker og ser bra ut.</p>
         <p>Jeg jobber mest med moderne webteknologi. 
             Next.js og Typescript er verktøyene jeg bruker for tiden. 
             Jeg liker å forstå hvordan ting fungerer under panseret, ikke bare få det til å virke.</p>
         <p>Jeg elsker å utforske skjæringspunktet mellom design, teknologi og AI.</p>
-    </main>
+    </PageLayout>
 );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,22 +1,20 @@
+import PageLayout from "../components/PageLayout";
 import ProjectCard from "../components/ProjectCard";
 import { projects } from "../data";
 
 export default function Projects() {
-    
-    return (
-        <main>
-            <h1>Prosjekter</h1>
-            <div className="flex flex-col gap-4 mt-6">
-            {projects.map((project) => (
-                <ProjectCard 
-                key={project.title}
-                slug={project.slug}
-                title={project.title}
-                description={project.description}
-                />
-                    ))}
-                    </div>
-        </main>
-    );
-
+  return (
+    <PageLayout title={<h1>Prosjekter</h1>}>
+      <div className="flex flex-col gap-4">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.title}
+            slug={project.slug}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
+      </div>
+    </PageLayout>
+  );
 }
